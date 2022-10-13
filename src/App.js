a702a510ecd0dabedf9f47a860ac0d11df55251a
Madeline Burns <mburns6023@gmail.com>
2022-10-13 17:37:22 -0400

    handled front end technologies for form submitting

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
 import Gallery from './components/Gallery';
-import ContactForm from './components/ContactForm';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
